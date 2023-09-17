a4bad2abda5bac15dfdabe677f3a175751513b2c
Infelu <kubanec04@seznam.cz>
2023-09-17 20:26:14 +0200

    Kalkulacka

---
+++ Calculator/namespace Calculator.cs
@@ -0,0 +1,54 @@
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Zadej prvni cislo: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej druhe cislo: ");
+            int y = int.Parse(Console.ReadLine());
+            CalculatorContext calc = new CalculatorContext(new OpAdd());
+            Console.WriteLine("OpAdd: "+calc.Execute(x, y));
+            calc.SetOperator(new OpSub());
+            Console.WriteLine("OpSub: "+calc.Execute(x, y));
+            calc.SetOperator(new OpMagic());
+            Console.WriteLine("OpMagic: "+calc.Execute(x, y));
+        }
+    }
+
+    public interface IOperator {
+        double Execute (int a, int b);
+    }
+
+    public class OpAdd : IOperator {
+        public double Execute (int a, int b){
+            return a+b;
+        }
+    }
+    public class OpSub : IOperator {
+        public double Execute (int a, int b){
+            return a-b;
+        }
+    }
+    public class OpMagic : IOperator {
+        public double Execute (int a, int b){
+            return a*b;
+        }
+    }
+    public class CalculatorContext {
+        private IOperator ioperator;
+        
+        public CalculatorContext(IOperator ioperator) {
+            this.ioperator = ioperator;
+        }
+
+        public void SetOperator(IOperator ioperator) {
+            this.ioperator = ioperator;
+        }
+
+        public double Execute(int a, int b) {
+            return ioperator.Execute(a, b);
+        }
+    }
+}
